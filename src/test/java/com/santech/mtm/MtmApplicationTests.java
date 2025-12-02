25a54487bf555367f4062c99d4fbc2b6a9bfc28e
Santiago <santiagofidelibus19@gmail.com>
2025-12-02 15:56:10 -0300

    First commit!
    Se crea la estructura inicial del proyecto.

---
+++ src/test/java/com/santech/mtm/MtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santech.mtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
