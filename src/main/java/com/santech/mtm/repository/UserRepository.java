f2cb50237e70df28a2c6b9788640f0d43e54f7d4
Santiago <santiagofidelibus19@gmail.com>
2025-12-02 20:53:46 -0300

    Se agregaron diversos metodos al repository

--- src/main/java/com/santech/mtm/repository/UserRepository.java
+++ src/main/java/com/santech/mtm/repository/UserRepository.java
@@ -2,10 +2,19 @@ package com.santech.mtm.repository;
 
 import com.santech.mtm.model.UserApp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository <UserApp, Long> {
 
+    Optional<UserApp> findByEmailAndActiveTrue(String email);
     Optional<UserApp> findByEmail(String email);
+    List<UserApp> findAllByActiveTrue();
+    Optional<UserApp> findByIdAndActiveTrue(Long id);
+
+
 }
